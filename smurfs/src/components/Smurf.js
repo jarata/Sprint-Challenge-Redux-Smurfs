f1e2fce71354c15584bb0c0855a2c407f124f120
J A <jarata@gmail.com>
2019-02-08 09:42:57 -0800

    some basic style

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Smurf = props => {
     return (
-        <div>
+        <div className="smurfs-card">
             <p>{props.smurf.name} Smurf</p>
             <p>{props.smurf.age} years old</p>
             <p>{props.smurf.height}</p>
